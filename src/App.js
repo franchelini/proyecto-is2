f55176685a9080f4563d11ba8242112f4d62f2e2
esck000 <fvcaesck0@gmail.com>
2025-05-01 21:31:32 -0400

    Eliminación bordes blancos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CssBaseline } from '@mui/material'; 
 import AppRoutes from './routes/AppRoutes';
 import CustomAppBar from './components/CustomAppBar';
 import Footer from './components/Footer';
 import Background from './components/Background';
 
-
 const App = () => {
   return (
     <Router>
+      <CssBaseline /> {/* Elimina estilos predeterminados del navegador */}
       <Background>
         <div
           style={{
             display: 'flex',
             flexDirection: 'column',
             minHeight: '100vh', // Asegura que ocupe toda la altura de la ventana
+            margin: 0, // Asegura márgenes en 0
+            padding: 0, // Asegura paddings en 0
           }}
         >
           <CustomAppBar />
